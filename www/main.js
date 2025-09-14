dcd779478b48ab723cfe18d83ed6b602fee277e1
Subrata Dey <subratadey@Subratas-MacBook-Air.local>
2025-09-14 15:40:28 +0530

    Initial commit

---
+++ www/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.h1').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: 'bounceIn',
+        },
+        out:{
+            effect: 'bounceOut',
+        }
+    })
+});
